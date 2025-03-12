e15b4dd7fd69bf9d2ddfda78b67e0ce2e2673abe
Edgar Sayohm Takamura <nightcorebrasil280@gmail.com>
2025-03-12 08:48:01 -0300

    Release 1.0
    Will be testing on how this release performs.
    
    Signed: Roxanne - 08:50

--- Linux Scripts/RenderFile.sh
+++ Linux Scripts/RenderFile.sh
@@ -1,5 +1,26 @@
 #This need to be reviewd
 #Find a good approach for this method
 
-read -p "Enter the path to the blend file: " input
-echo "Filepath - "$input
+#Here we move the INPUT file from wherever it is to the ./Input folder
+#Use this script in case you have your file stored somewhere else on your machine OUTSIDE the root folder (./path/to/BlenderDocker)
+#Hell even if you want to move from a folder inside it as well
+#read -p Is the File, ZIPPED'(.zip)'? result
+#if test "$result" = "Yes" || test "$result" = "Y"; then
+#    DECOMPRESS=true
+#fi
+#read -p "Enter the path to the blend file: " input
+#echo "Filepath - "$input
+
+#if [ $DECOMPRESS ]; then
+#    echo "Unzipping File"
+#    mkdir "../input/UnzippedFile"
+#else
+#    echo "Unzipping disabled"
+#fi
+
+#read -p "Enter the path to the blend file: " input
+#echo "Filepath - "$input
+
+#Refactor needed, maybe a C# application is better than a shell sript
+
+docker compose run render "/usr/local/blender/blender "-noaudio" "--background" "${.}/${render_input_file}"
